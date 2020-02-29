9aeafa28f86faa097f59a3986befd893c0a1c485
Tobias Olausson <tobias.a.olausson@gmail.com>
2020-02-29 16:47:28 +0100

    Add base

---
+++ Assets/Scripts/Mechanics/Player1Controller.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Platformer.Gameplay;
+using static Platformer.Core.Simulation;
+using Platformer.Model;
+using Platformer.Core;
+
+namespace Platformer.Mechanics
+{
+    /// <summary>
+    /// This is the main class used to implement control of the player.
+    /// It is a superset of the AnimationController class, but is inlined to allow for any kind of customisation.
+    /// </summary>
+    public class Player1Controller : PlayerController
+    {
+
+        protected override void Update()
+        {
+            if (controlEnabled)
+            {
+                move.x = Input.GetAxis("Horizontal");
+                if (jumpState == JumpState.Grounded && Input.GetButtonUp("Jump"))
+                    jumpState = JumpState.PrepareToJump;
+                else if (Input.GetButtonUp("Jump"))
+                {
+                    stopJump = true;
+                    Schedule<PlayerStopJump>().player = this;
+                }
+            }
+            else
+            {
+                move.x = 0;
+            }
+            UpdateJumpState();
+            base.Update();
+        }
+    }
+}
